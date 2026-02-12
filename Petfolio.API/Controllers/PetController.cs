fc672514c5a1f52d1dcb87fe25b1af4c99fce055
w-jayson <weslley.jayson08@outlook.com>
2026-02-12 16:22:36 -0300

    adicionado endpoint de edição

--- Petfolio.API/Controllers/PetController.cs
+++ Petfolio.API/Controllers/PetController.cs
@@ -1,6 +1,7 @@
 
 using Microsoft.AspNetCore.Mvc;
 using Petfolio.Application.UseCases.Pets.Register;
+using Petfolio.Application.UseCases.Pets.Update;
 using Petfolio.Communication.Requests;
 using Petfolio.Communication.Responses;
 
@@ -10,13 +11,24 @@ namespace Petfolio.API.Controllers
     [Route("api/[controller]")]
     public class PetController : ControllerBase
     {
-       [HttpPost]
-       [ProducesResponseType(typeof(ResponseRegisterPetJson), StatusCodes.Status201Created)]
-       public IActionResult Register([FromBody] RequestRegisterPetJson request)
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisterPetJson), StatusCodes.Status201Created)]
+        public IActionResult Register([FromBody] RequestRegisterPetJson request)
         {
             var response = new RegisterPetUseCase().Execute(request);
 
             return Created(string.Empty, response);
-        } 
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Update([FromRoute] int id, [FromBody] RequestRegisterPetJson request)
+        {
+            var useCase = new UpdatePetUseCase();
+            useCase.Execute(id, request);
+
+            return NoContent();
+        }  
     }
 }
---
+++ Petfolio.Application/UseCases/Pets/Update/UpdatePetUseCase.cs
@@ -0,0 +1,12 @@
+using Petfolio.Communication.Requests;
+
+namespace Petfolio.Application.UseCases.Pets.Update
+{
+    public class UpdatePetUseCase
+    {
+        public void Execute(int id, RequestRegisterPetJson request)
+        {
+            
+        }
+    }
+}
